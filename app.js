0215ff8ee806e844758c6ae42f5f287256815609
Asael Enrique de la Rosa <asaelrs20@gmail.com>
2021-01-13 16:13:44 -0500

    auth0

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // -------------------------------------------------
-
-const config = require('config')
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+const config = require('config');
 const express = require('express');
 const app = express();
 
@@ -10,11 +11,24 @@ const routes = require('./routes/inex');
 require('./data/connection');
 //-------------------------------------------------
 
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-7z5dlz6h.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'http://localhost:3000/service.cards/api/',
+  issuer: 'https://dev-7z5dlz6h.us.auth0.com/',
+  algorithms: ['RS256']
+});
+
 
 const domain = config.get('server.domain');
 
 app.set('view engine', 'hbs')
 
+app.use(jwtCheck);
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
